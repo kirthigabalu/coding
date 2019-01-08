51890bc1dffc0d4b8b337df96ea225c10736d402
kirthigabalu <46441630+kirthigabalu@users.noreply.github.com>
2019-01-08 21:53:50 +0530

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -3,7 +3,7 @@ if x>="a" and x<="z" or x>="A" and x<="Z":
     if x=="a" or x=="e" or x=="i" or x=="o" or x=="u":
        print("Vowels")
     elif x=="A" or x=="E" or x=="I" or x=="O" or x=="U":
-       print("Vowels")
+       print("Vowel")
     else:
        print("Consonant")
 else:
