394c4f81593dea7ef0993643af5a0e45003d1838
kirthigabalu <46441630+kirthigabalu@users.noreply.github.com>
2019-01-08 21:45:56 +0530

    Create vowels.py

---
+++ vowels.py
@@ -0,0 +1,10 @@
+x=input()
+if x>="a" and x<="z" or x>="A" and x<="Z":
+    if x=="a" or x=="e" or x=="i" or x=="o" or x=="u":
+       print("Vowels")
+    elif x=="A" or x=="E" or x=="I" or x=="O" or x=="U":
+       print("Vowels")
+    else:
+       print("Constant")
+else:
+    print("Invalid")
