05556763c9bf2b7c94bd04d21244592f7b37d0f6
kirthigabalu <46441630+kirthigabalu@users.noreply.github.com>
2019-01-26 17:15:06 +0530

    Update armstrongno.py

--- armstrongno.py
+++ armstrongno.py
@@ -1,11 +1,15 @@
-x=int(input())
-y=x
-sum=0
-while x>0:
-    r=x%10
-    sum=sum+r*r*r
-    x=x//10
-if sum==y:
-    print("yes")
-else:
-    print("no")
+x,y=map(int,input().split())
+l=[]
+for i in range(x+1,y):
+    p=i
+    sum=0
+    while i>0:
+        r=i%10
+        sum=sum+r*r*r
+        i=i//10
+    if sum==p:
+        l.append(p)
+for j in range(len(l)-1):
+    print(l[j],end=" ")
+print(l[-1])
+
