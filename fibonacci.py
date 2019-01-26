47f292643946453a59ae4676be65d2190770bdd8
kirthigabalu <46441630+kirthigabalu@users.noreply.github.com>
2019-01-26 19:39:19 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,17 @@
+n=int(input())
+l=[]
+i=0
+frst=0
+sec=1
+while i<n:
+    if i<=1:
+        next=i
+    else:
+        next=frst+sec
+        frst=sec
+        sec=next
+    l.append(next)
+    i=i+1
+for j in range(len(l)-1):
+    print(l[j],end=" ")
+print(l[-1])
