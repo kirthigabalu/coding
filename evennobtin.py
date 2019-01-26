bf1c93e82b1fb167c6992cba3ccf9a364ade5556
kirthigabalu <46441630+kirthigabalu@users.noreply.github.com>
2019-01-26 16:32:50 +0530

    Update evennobtin.py

--- evennobtin.py
+++ evennobtin.py
@@ -1,5 +1,9 @@
 x,y=map(int,input().split())
+l=[]
 for i in range(x+1,y):
     if i%2==0:
-        print(i,end=" ")
-        
+        l.append(i)
+for j in range(len(l)-1):
+    print(l[j],end=" ")
+print(l[-1])
+
