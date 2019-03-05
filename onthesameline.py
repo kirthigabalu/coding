d2af8f15a1a4df4ffaae6fec2b78e93baba3923b
kirthigabalu <46441630+kirthigabalu@users.noreply.github.com>
2019-03-05 15:30:59 +0530

    Update onthesameline.py

--- onthesameline.py
+++ onthesameline.py
@@ -5,7 +5,7 @@ if l[0]==s[0] and s[0]==t[0] and t[0]==l[0]:
 	print("yes")
 elif l[1]==s[1] and s[1]==t[1] and t[1]==l[1]:	
 	print("yes")
-elif l[0]==l1[1] and s[0]==s[1] and t[0]==t[1]:
+elif l[0]==l[1] and s[0]==s[1] and t[0]==t[1]:
 	print("yes")
 else:
 	print("no")
