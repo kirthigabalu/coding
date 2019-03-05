a2ab2f703befffe5348646997408ac537673e4c3
kirthigabalu <46441630+kirthigabalu@users.noreply.github.com>
2019-03-05 12:48:54 +0530

    Create prblm83.py

---
+++ prblm83.py
@@ -0,0 +1,14 @@
+#problem of 83
+n=input()
+if "/" in n:
+	d=n.index("/")
+	a=int(n[:d])
+	b=int(n[d+1:])
+	c=a//b
+	print(c)
+elif "%" in n:
+	d=n.index("%")
+	a=int(n[:d])
+	b=int(n[d+1:])
+	c=a%b
+	print(c)
