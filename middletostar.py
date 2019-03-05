a09e9a96a38ced654eaa925cfaa9808272eb94de
kirthigabalu <46441630+kirthigabalu@users.noreply.github.com>
2019-03-05 12:14:50 +0530

    Create middletostar.py

---
+++ middletostar.py
@@ -0,0 +1,13 @@
+#changing middle term into star
+s=input()
+l=list(s)
+m=len(l)//2
+if len(l)%2==0:
+	l[m]="*"
+	l[m-1]="*"
+	s1="".join(l)
+	print(s1)
+else:
+	l[m]="*"
+	s2="".join(l)
+	print(s2)
