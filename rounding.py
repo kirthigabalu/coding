d6143273ea3fb445fbb749d1d85fa66503f3730f
kirthigabalu <46441630+kirthigabalu@users.noreply.github.com>
2019-03-05 11:57:40 +0530

    Create rounding.py

---
+++ rounding.py
@@ -0,0 +1,18 @@
+#roundoff in decimal
+n=float(input())
+n1=int(n)
+s=n1+1
+print(s)
+75
+s=input()
+l=list(s)
+m=len(l)//2
+if len(l)%2==0:
+	l[m]="*"
+	l[m-1]="*"
+	s1="".join(l)
+	print(s1)
+else:
+	l[m]="*"
+	s2="".join(l)
+	print(s2)
