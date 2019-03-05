71447d8d80a5c45d7281c31ec47f3475de65e6d0
kirthigabalu <46441630+kirthigabalu@users.noreply.github.com>
2019-03-05 14:36:35 +0530

    Create converttoint.py

---
+++ converttoint.py
@@ -0,0 +1,5 @@
+k=input()
+j=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XXX']
+for i in range(0,len(j)):
+	if k==j[i]:
+		print(i+1)
