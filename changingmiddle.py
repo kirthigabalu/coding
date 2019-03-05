3ccd57afd0cccd33ee0bd05f98c4f176e8766c04
kirthigabalu <46441630+kirthigabalu@users.noreply.github.com>
2019-03-05 11:59:55 +0530

    Create changingmiddle.py

---
+++ changingmiddle.py
@@ -0,0 +1,12 @@
+#changing the middle element 
+n=int(input())
+c=0
+for i in range(2,n-1):
+    if n%i==0:
+       c=c+1
+    else:
+       continue
+if c==0:
+    print("no")
+else:
+    print("yes")
