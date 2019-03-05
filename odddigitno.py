69840673852770553adde733dc75939a10f1f100
kirthigabalu <46441630+kirthigabalu@users.noreply.github.com>
2019-03-05 12:23:37 +0530

    Create odddigitno.py

---
+++ odddigitno.py
@@ -0,0 +1,12 @@
+#odd number digit will be displayed
+x=int(input())
+l=[]
+while x:
+	rem=x%10
+	if rem%2==1:
+		l.append(rem)
+	x=x//10
+l.reverse()
+for i in range(0,len(l)-1):
+	print(l[i],end=" ")
+print(l[-1])
